609f010fcb3eaada304fe17515f0a9408e19f717
lilwizzi9 <62917075+lilwizzi9@users.noreply.github.com>
2022-11-13 15:46:24 +0100

    ehellooo !!

---
+++ api/calc_extange.php
@@ -0,0 +1,44 @@
+<?php
+include "db_lib.php";
+
+
+
+
+function getExtange(string $peer, int $amount)
+{
+    global $conn;
+    $peer = $conn->escape_string($peer);
+    $amount = $conn->escape_string($amount);
+    $ext = run_query_single("SELECT `value` FROM `gen_custom` WHERE `property`='$peer'")['value'];
+    if($ext){
+        return $amount*$ext;
+    }else{
+        return "0...";
+ 
+    }
+    # code...
+}
+
+
+if(isset($_POST['action'])){
+    // TODO Js response
+    if($_POST['action']=='ext'){
+        if(isset($_POST['amount'],$_POST['peer'])){
+            $peer = $_POST['peer'];
+            $amount = $_POST['amount'];
+            echo getExtange($peer, $amount);
+        }else{
+            echo '0...';
+        }
+    }
+}
+
+
+
+
+
+?>
+
+
+
+
---
+++ api/update.js
@@ -0,0 +1,25 @@
+
+// function httpGet(theUrl)
+// {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+//     xmlHttp.send( null );
+//     return xmlHttp.responseText;
+// }
+
+function update_extange_rate(peer,amount)
+{
+
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "POST", "../api/calc_extange.php", false ); // false for synchronous request
+    xmlHttp.onload = function() {
+        return(this.responseText)  
+        // If you wanted to call the function in here, then just make another whole xhr var and send it in this function
+    }
+    xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    //xmlHttp.send();
+    xmlHttp.send("action=ext&amount="+amount+"&peer="+peer );
+
+}
+
