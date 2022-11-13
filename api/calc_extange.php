0d109b023fc3a65335cb8213a65603c810be2048
decredwatchman <emmanuel09073048453@gmail.com>
2022-11-13 23:11:31 +0100

    pusded

--- api/calc_extange.php
+++ api/calc_extange.php
@@ -6,15 +6,14 @@ include "db_lib.php";
 
 function getExtange(string $peer, int $amount)
 {
-    
     global $conn;
     $peer = $conn->escape_string($peer);
     $amount = $conn->escape_string($amount);
     $ext = run_query_single("SELECT `value` FROM `gen_custom` WHERE `property`='$peer'")['value'];
     if($ext){
         return $amount*$ext;
-    }if($ext ==""){
-        return 0;
+    }else{
+        return "0...";
  
     }
     # code...
@@ -26,6 +25,10 @@ if(isset($_POST['action'])){
 
     if($_POST['action']=='ext'){
         if(isset($_POST['amount'],$_POST['peer'])){
+           if($_POST['amount']==""){
+            echo 0;
+            exit;
+           }
             $peer = $_POST['peer'];
             $amount = $_POST['amount'];
             echo getExtange($peer, $amount);
