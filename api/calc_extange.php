d5c382a7a82d45d84a07694079c3cbb55786a5bc
lilwizzi9 <62917075+lilwizzi9@users.noreply.github.com>
2022-11-13 23:33:32 +0100

    ertyuiop

--- api/calc_extange.php
+++ api/calc_extange.php
@@ -20,6 +20,34 @@ function getExtange(string $peer, int $amount)
 }
 
 
+function get_usdt_Bal()
+{
+    $usr = get_userid();
+    
+    $usdt = run_query_single("SELECT `value` FROM `usr_$usr` WHERE `property`='usdt'");
+
+    if($usdt){
+        return $usdt['value'];
+    }
+    return 0;
+    
+}
+function get_busd_Bal()
+{
+    $usr = get_userid();
+    
+    $busd = run_query_single("SELECT `value` FROM `usr_$usr` WHERE `property`='busd'");
+
+    if($busd){
+        return $busd['value'];
+    }
+    return 0;
+    
+}
+
+
+
+
 if(isset($_POST['action'])){
     //TODO Js response
 
@@ -30,6 +58,23 @@ if(isset($_POST['action'])){
             echo getExtange($peer, $amount);
         }else{
             echo '0...';
+        }
+    }
+    if($_POST['action']=='busd'){
+        get_busd_Bal();
+    }
+    if($_POST['action']=='usdt'){
+        get_usdt_Bal();
+    }
+    // action = bal_enough, from busd/usdt, amount
+    if($_POST['action']=='bal_enough'){
+        if(isset($_POST['from'],$_POST['amount'])){
+            if($_POST['from']=='busd/usdt' ){
+                echo get_busd_Bal()>=$_POST['amount'];
+            }else{
+                echo get_usdt_Bal()>=$_POST['amount'];
+            }
+
         }
     }
 }
