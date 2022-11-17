1c072064590f7de2cb1038526960adadb119ca5c
decredwatchman <emmanuel09073048453@gmail.com>
2022-11-17 10:41:09 +0100

    push

--- register.php
+++ register.php
@@ -54,7 +54,7 @@ if(isLoggedin()){
             <input type="text" placeholder="username" name="username" id="" required>
             <input type="email" placeholder="Email address" name="email" id="" required>
         
-            <input type="password" placeholder="Password" name="password" id="pass" required>
+            <input type="password" placeholder="Password" name="password" id="pass" minlenght="8" required>
             <span id="wrong_pass_alert"></span>
 
             <input type="password" placeholder="confirm Password" name="" id="confirm_pass"  onkeyup="validate_password()" required>
